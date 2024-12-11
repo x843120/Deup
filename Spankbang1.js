b8fd42795377f72f84d7cdf1482ca6f36656546e
Echo <1575672377@qq.com>
2024-12-11 20:44:41 +0800

    Update and rename Spankbang.js to Spankbang1.js

--- Spankbang1.js
+++ Spankbang1.js
@@ -39,8 +39,31 @@ async list(object, offset, limit) {
   const response = await $axios.get(url, {
     headers: {
       'Cookie': this._cookie,
-      'Referer': 'https://spankbang.com/',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
+      ':authority': 'spankbang.com,
+      ':method': 'GET',
+      ':path': '/users/recommendations',
+      ':scheme': 'https',
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'zh-CN,zh;q=0.9',
+      'cache-control': 'max-age=0',
+      'priority': 'u=0, i',
+      'referer': 'https://spankbang.com/',
+      'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+      'sec-ch-ua-arch': "x86",
+      'sec-ch-ua-bitness': "64",
+      'sec-ch-ua-full-version': "131.0.2903.86",
+      'sec-ch-ua-full-version-list': '"Microsoft Edge";v="131.0.2903.86", "Chromium";v="131.0.6778.109", "Not_A Brand";v="24.0.0.0"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-model': "",
+      'sec-ch-ua-platform': "Windows",
+      'sec-ch-ua-platform-version': "15.0.0",
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'same-origin',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'
     },
   });
   return this.parseVideoList(response.data);
